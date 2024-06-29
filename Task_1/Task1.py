82f0f61195bf22d8f5e5f1b3547f8c4e49215ecb
mohammad9yamani <52174742+mohammad9yamani@users.noreply.github.com>
2024-06-29 13:07:18 +0300

    Task_1

---
+++ Task_1/Task1.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import pytesseract
+import sys
+
+def extract_text(image_path):
+    
+    with Image.open(image_path) as img:
+        text = pytesseract.image_to_string(img)
+    return text
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python extract_text.py <image_path>")
+        sys.exit(1)
+
+    image_path = sys.argv[1]
+    try:
+        extracted_text = extract_text(image_path)
+        print("Extracted Text:")
+        print(extracted_text)
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
